20a89330a9bcea9f5c83d80e76fafa2b44af0302
Jeff Garcia <jeffrgarcia24@gmail.com>
2016-01-10 16:58:24 -0700

    small changes

--- js/blog/blogService.js
+++ js/blog/blogService.js
@@ -15,6 +15,18 @@ angular.module('myApp')
       author: 'Britt Garcia',
       date: 'January 2, 2016',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam hendrerit enim eget velit lacinia scelerisque. Sed dictum porta auctor. Aenean ut dolor vitae lectus sodales sagittis. Ut malesuada imperdiet nisi et faucibus. Quisque mi ligula, sagittis ut nunc eget, bibendum interdum erat. Integer congue purus et quam condimentum, sed malesuada tellus mollis. Vestibulum elementum justo sit amet nibh tincidunt dapibus.'
+    },
+      {
+      title: 'Second Blog',
+      author: 'Britt Garcia',
+      date: 'January 2, 2016',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam hendrerit enim eget velit lacinia scelerisque. Sed dictum porta auctor. Aenean ut dolor vitae lectus sodales sagittis. Ut malesuada imperdiet nisi et faucibus. Quisque mi ligula, sagittis ut nunc eget, bibendum interdum erat. Integer congue purus et quam condimentum, sed malesuada tellus mollis. Vestibulum elementum justo sit amet nibh tincidunt dapibus.'
+    },
+      {
+      title: 'Second Blog',
+      author: 'Britt Garcia',
+      date: 'January 2, 2016',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam hendrerit enim eget velit lacinia scelerisque. Sed dictum porta auctor. Aenean ut dolor vitae lectus sodales sagittis. Ut malesuada imperdiet nisi et faucibus. Quisque mi ligula, sagittis ut nunc eget, bibendum interdum erat. Integer congue purus et quam condimentum, sed malesuada tellus mollis. Vestibulum elementum justo sit amet nibh tincidunt dapibus.'
     }
   ];
 
